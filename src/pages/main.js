75e1fea1b10e1162b43a88c6a149cc678849ff12
vrcxo-tech <68752022+vrcxo-tech@users.noreply.github.com>
2022-02-02 00:56:07 +0300

    first commit of the user interface

---
+++ src/pages/main.js
@@ -0,0 +1,36 @@
+import "../styles/main.css";
+import React, {useEffect,useState } from "react";
+const Axios = require("axios");
+
+const Main = () => {
+  const [cryptos, setCryptos] = useState([]);
+  const showCryptos = () =>
+    Axios.get("http://localhost:3003/cryptoprice").then((response) => {
+      let crypto = response.data;
+        setCryptos(crypto)
+    });
+  useEffect(showCryptos,[cryptos])
+  return (
+    
+    <main className='assets-holder'>
+      <header><h1>CryptoPrice.io</h1></header>
+      {cryptos.map((val) => {
+        const { cryptoName, price, marketCap, volume, circulatingSupply,graph,cryptoLogo} = val;
+        return (
+          <section id='assets'>
+            <img id="logo" src={cryptoLogo} alt='' />
+            <h2>{cryptoName}</h2>
+            <img src={graph} alt='' />
+            <p>MarketCap: {marketCap}</p>
+            <p id="price">Price: {price}</p>
+            <p>Volume: {volume}</p>
+            <p id="supply">Circulating Supply: {circulatingSupply}</p>
+          </section>
+        );
+      }).slice(0,10)}
+    </main>
+      
+  );
+};
+
+export default Main;
