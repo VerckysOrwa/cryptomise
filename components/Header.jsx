3b2ba5fc48d99572fde6e53631f23dd24ed95bff
Verckys Orwa <68752022+VerckysOrwa@users.noreply.github.com>
2022-02-04 11:35:27 +0300

    Add files via upload

---
+++ components/Header.jsx
@@ -0,0 +1,29 @@
+
+import React from 'react';
+import "../styles/header.css"
+import { FaHome,FaCoins, FaNewspaper } from "react-icons/fa";
+import {Link} from 'react-router-dom'
+
+const Header = () => {
+    return(
+         <header>
+        <h1>CryptoPrice</h1>
+<Link to="/" id="home-nav"><FaHome id="home-icon"/></Link>
+        <section id='news-nav'>
+            <Link to='/businessnews'>
+          <h3>
+              Business News <FaNewspaper />
+          </h3>
+          </Link>
+          <Link to="/nfts">
+          <h3>
+            NFTs <FaCoins />
+            </h3>
+          </Link>
+        </section>
+
+      </header>
+    )
+};
+
+export default Header;
---
+++ pages/main.js
@@ -0,0 +1,48 @@
+import "../styles/main.css";
+import React, {useEffect,useState } from "react";
+import Header from "../components/Header";
+const Axios = require("axios");
+
+const Main = () => {
+  const [cryptos, setCryptos] = useState([]);
+  const showCryptos = () =>
+    Axios.get("https://cryptolize.herokuapp.com/cryptoprice").then(
+      (response) => {
+        let crypto = response.data;
+        setCryptos(crypto);
+      }
+    );
+  useEffect(showCryptos,[cryptos])
+  return (
+    <main className='assets-holder'>
+     <Header/>
+
+      {cryptos
+        .map((val) => {
+          const {
+            cryptoName,
+            price,
+            marketCap,
+            volume,
+            circulatingSupply,
+            graph,
+            cryptoLogo,
+          } = val;
+          return (
+            <section id='assets'>
+              <img id='logo' src={cryptoLogo} alt='' />
+              <h2>{cryptoName}</h2>
+              <img src={graph} alt='' />
+              <p>MarketCap: {marketCap}</p>
+              <p id='price'>Price: {price}</p>
+              <p>Volume: {volume}</p>
+              <p id='supply'>Circulating Supply: {circulatingSupply}</p>
+            </section>
+          );
+        })
+        .slice(0, 10)}
+    </main>
+  );
+};
+
+export default Main;
