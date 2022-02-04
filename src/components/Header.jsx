129d235c723793074705bd630cb85fa5c203568f
Verckys Orwa <68752022+VerckysOrwa@users.noreply.github.com>
2022-02-04 11:42:46 +0300

    Changed the name from "cryptoprice" to  "cryptomise"

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -7,7 +7,7 @@ import {Link} from 'react-router-dom'
 const Header = () => {
     return(
          <header>
-        <h1>CryptoPrice</h1>
+        <h1>Cryptomise</h1>
 <Link to="/" id="home-nav"><FaHome id="home-icon"/></Link>
         <section id='news-nav'>
             <Link to='/businessnews'>
